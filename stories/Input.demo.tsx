5cf34f04922396dc08e73fee9947f8fb7bd600d4
Adam Laker Illoul <85499621+Ademsk1@users.noreply.github.com>
2023-10-23 09:58:23 +0100

    fix: inf re-render

--- stories/Input.demo.tsx
+++ stories/Input.demo.tsx
@@ -2,26 +2,18 @@ import { Input } from "@/components/Input";
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
 export const InputDemo = ({ starterText = "" }) => {
-  const [input, setInput] = useState({ textInput: starterText, warning: true });
+  const [input, setInput] = useState(starterText);
+  const warning = input === "" ? true : false;
   const handleOnChange = (event: React.FormEvent<HTMLInputElement>) => {
-    setInput({ ...input, textInput: event.currentTarget.value });
+    setInput(event.currentTarget.value);
   };
-  useEffect(() => {
-    if (input.textInput !== "") {
-      setInput({ ...input, warning: false });
-    } else {
-      setInput({ ...input, warning: true });
-    }
-  }, [input]);
 
   return (
     <Input
-      value={input.textInput}
+      value={input}
       onChange={handleOnChange}
       placeholder="My placeholder text"
-      className={cn(
-        input.warning && "bg-red-300 border-red-400 placeholder-red-600"
-      )}
+      className={cn(warning && "bg-red-300 border-red-400 placeholder-red-600")}
     />
   );
 };
